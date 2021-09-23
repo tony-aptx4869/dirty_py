3f6697758e1900cb2592a50857dda0e0b55a52f0
Tony Chang <tonychang7869@gmail.com>
2021-09-23 10:46:49 +0800

    Create generate_ffmpeg_files_txt.py

---
+++ generate_ffmpeg_files_txt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# @ Tony Chang
+# Copyright (c) 1994-2021 Tony Chang \
+# https://github.com/tony-aptx4869 \
+# https://aptx4869.tv \
+# All Rights Reserved.
+
+filenumber = 1
+
+while filenumber <= 270 :
+  print('file \'%d.ts\'' %filenumber)
+  filenumber += 1
+
